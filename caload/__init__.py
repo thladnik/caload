267589e4d70a58313a27662304aeff5ebc69d137
thladnik <tim.github@hladnik.de>
2024-07-31 18:30:34 +0200

    Add header information;

--- caload/__init__.py
+++ caload/__init__.py
@@ -2,6 +2,15 @@ from __future__ import annotations
 
 from caload.analysis import open_analysis
 
+__author__ = 'Tim Hladnik'
+__copyright__ = 'Copyright 2024'
+__license__ = 'GPLv3'
+__version__ = '0.1.0'
+__maintainer__ = 'Tim Hladnik'
+__email__ = 'tim.github@hladnik.de'
+__status__ = 'Development'
+
+
 __all__ = ['sqltables', 'entities', 'digest', 'analysis', 'open_analysis', 'utils','s2p_autorun',
            'less', 'lessequal', 'equal', 'greaterequal', 'greater', 'is_true', 'is_false']
 
